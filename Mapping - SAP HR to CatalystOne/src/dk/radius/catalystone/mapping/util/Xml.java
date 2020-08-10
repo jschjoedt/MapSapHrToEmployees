e2d5fd1634ccdb89e40f12b77d03d3464327ef73
XJEHJ <XJEHJ@PC81285.de-prod.dk>
2020-08-10 10:39:51 +0200

    Refactoring

--- Mapping - SAP HR to CatalystOne/src/dk/radius/catalystone/mapping/util/Xml.java
+++ Mapping - SAP HR to CatalystOne/src/dk/radius/catalystone/mapping/util/Xml.java
@@ -41,38 +41,61 @@ public class Xml {
 		for (DO_E1PITYP type : hrmd_a07.IDOC.E1PLOGI.E1PITYP) {
 
 			if (type.INFTY.equals("0000")) {
-				employee = createFildInfo("1131", type.E1P0000.PERNR, employee);
-				employee = createFildInfo("1029", type.E1P0000.MASSN, employee);
-				
+				employee = mapInfoType0000(employee, type);
+
 			} else if (type.INFTY.equals("0001")) {
-				employee = createFildInfo("1105", type.E1P0001.KOSTL, employee);
-				employee = createFildInfo("8", type.E1P0001.ORGEH, employee);
-				employee = createFildInfo("1098", type.E1P0001.PLANS, employee);
-				employee = createFildInfo("1086", type.E1P0001.ZZTRACK, employee);
-				employee = createFildInfo("1071", type.E1P0001.BUKRS, employee);
-				employee = createFildInfo("1132", type.E1P0001.WERKS, employee);
-				employee = createFildInfo("??", type.E1P0001.ZZPLEVEL, employee);
-				employee = createFildInfo("1086", type.E1P0001.ZZCLEVEL, employee);
-				employee = createFildInfo("1100", type.E1P0001.PERSG, employee);
-				employee = createFildInfo("1101", type.E1P0001.PERSK, employee);
-			
+				employee = mapInfoType0001(employee, type);
+
 			} else if (type.INFTY.equals("0002")) {
-				employee = createFildInfo("1092", type.E1P0002.INITS, employee);
-				employee = createFildInfo("2", type.E1P0002.NACHN, employee);
-				employee = createFildInfo("26", type.E1P0002.NACH2, employee);
-				employee = createFildInfo("3", type.E1P0002.VORNA, employee);
-				employee = createFildInfo("19", type.E1P0002.GESCH, employee);
-				employee = createFildInfo("21", type.E1P0002.GBDAT, employee);
-				employee = createFildInfo("1089", type.E1P0002.NATIO, employee);
-				
-			} else if (type.INFTY.equals("0105") && type.SUBTY.equals("9950")) {
-				employee.GUID = type.E1P0105.get(0).USRID_LONG;
-			} else if (type.INFTY.equals("0105") && type.SUBTY.equals("0010")) {
-				//employee = createFildInfo("7", type.E1P0105.get(0).USRID_LONG, employee);
+				employee = mapInfoType0002(employee, type);
+
+			} else if (type.INFTY.equals("0105")) {
+				employee = mapInfoType0105(employee, type);
 			}
 		}
 
 		return employees;
+	}
+
+	private static DO_EMPLOYEE mapInfoType0105(DO_EMPLOYEE employee, DO_E1PITYP type) {
+		if (type.SUBTY.equals("9950")) {
+			employee.GUID = type.E1P0105.get(0).USRID_LONG;
+		} else if (type.SUBTY.equals("0010")) {
+			employee = createFildInfo("7", type.E1P0105.get(0).USRID_LONG, employee);
+		}
+
+		return employee;
+	}
+
+	private static DO_EMPLOYEE mapInfoType0002(DO_EMPLOYEE employee, DO_E1PITYP type) {
+		employee = createFildInfo("1092", type.E1P0002.INITS, employee);
+		employee = createFildInfo("2", type.E1P0002.NACHN, employee);
+		employee = createFildInfo("26", type.E1P0002.NACH2, employee);
+		employee = createFildInfo("3", type.E1P0002.VORNA, employee);
+		employee = createFildInfo("19", type.E1P0002.GESCH, employee);
+		employee = createFildInfo("21", type.E1P0002.GBDAT, employee);
+		employee = createFildInfo("1089", type.E1P0002.NATIO, employee);
+		return employee;
+	}
+
+	private static DO_EMPLOYEE mapInfoType0001(DO_EMPLOYEE employee, DO_E1PITYP type) {
+		employee = createFildInfo("1105", type.E1P0001.KOSTL, employee);
+		employee = createFildInfo("8", type.E1P0001.ORGEH, employee);
+		employee = createFildInfo("1098", type.E1P0001.PLANS, employee);
+		employee = createFildInfo("1086", type.E1P0001.ZZTRACK, employee);
+		employee = createFildInfo("1071", type.E1P0001.BUKRS, employee);
+		employee = createFildInfo("1132", type.E1P0001.WERKS, employee);
+		employee = createFildInfo("??", type.E1P0001.ZZPLEVEL, employee);
+		employee = createFildInfo("1086", type.E1P0001.ZZCLEVEL, employee);
+		employee = createFildInfo("1100", type.E1P0001.PERSG, employee);
+		employee = createFildInfo("1101", type.E1P0001.PERSK, employee);
+		return employee;
+	}
+
+	private static DO_EMPLOYEE mapInfoType0000(DO_EMPLOYEE employee, DO_E1PITYP type) {
+		employee = createFildInfo("1131", type.E1P0000.PERNR, employee);
+		employee = createFildInfo("1029", type.E1P0000.MASSN, employee);
+		return employee;
 	}
 
 
@@ -92,10 +115,10 @@ public class Xml {
 	public static void write(DO_EMPLOYEES employees, OutputStream os) throws JAXBException {
 		JAXBContext jaxbContext = JAXBContext.newInstance(DO_EMPLOYEES.class);
 		Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
-		
+
 		jaxbMarshaller.setProperty("com.sun.xml.internal.bind.xmlHeaders", "<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
 		jaxbMarshaller.setProperty(Marshaller.JAXB_FRAGMENT, Boolean.TRUE);
-		
+
 		jaxbMarshaller.marshal(employees, os);
 
 	}
